a1154e00f21d5a525b4f300d2d1eab5ccd004a54
Ayush <ayushmandal0122@gmail.com>
2024-11-15 16:54:10 +0530

    initial commit

---
+++ src/Canvas.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import canvasImages from "./canvasImages";
+
+function Canvas() {
+    useEffect(() => {
+
+    });
+    return <canvas id="canvas" ></canvas>;
+}
+
+export default Canvas;
