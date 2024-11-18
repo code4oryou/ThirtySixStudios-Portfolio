abd88a34e088ef12234a2887255002b1c4a0c789
Ayush <ayushmandal0122@gmail.com>
2024-11-18 16:58:25 +0530

    landing page modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,45 @@ import { useEffect } from "react";
 
 function App() {
 
-  useEffect(()=>{
+  useEffect(() => {
     const locomotiveScroll = new LocomotiveScroll();
-  },[]);
-  
+  }, []);
+
   return (<>
-    <div className="w-full relative min-h-screen ">
-      {data[0].map((canvasdets, index) => ( //each row is an array of canvas details and here we are mapping over the first row
+    <div className="w-full relative min-h-screen font-['Helvetica_Now_Display']">
+      {/* {data[0].map((canvasdets, index) => ( //each row is an array of canvas details and here we are mapping over the first row
         <Canvas details={canvasdets} />
-      ))} 
+      ))} */}
+      <div className="w-full h-screen text-white">
+        <nav className="top-0 left-0 w-full p-8 flex justify-between z-50">
+          <div className="brand text-2xl font-md">thirtysixstudios</div>
+          <div className="links flex gap-10">
+            {[
+              "What we do",
+              "Who we are",
+              "How we give back",
+              "Talk to us",
+            ].map((link, index) => (
+              <a
+                key={index}
+                href={`#${link.toLowerCase()}`}
+                className="text-md hover:text-gray-300"
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+        </nav>
+        <div className="textcontainer w-full px-[20%]">
+          <div className="text w-[65%]">
+            <h3 className="text-4xl leading-[1.3]">
+              At Thirtysixstudio, we build immersive digital experiences for brands with a purpose.
+            </h3>
+            <p className="text-md w-[80%] mt-10" >We’re a boutique production studio focused on design, motion, and creative technology, constantly reimagining what digital craft can do for present-time ads and campaigns.</p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="w-full relative min-h-screen ">
-      {data[1].map((canvasdets, index) => ( //each row is an array of canvas details and here we are mapping over the second row
-        <Canvas details={canvasdets} />
-      ))} 
-    </div>
-    
   </>
   );
 }
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -3,9 +3,9 @@ import canvasImages from "./canvasImages";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-function Canvas({details}) {
-    const {startIndex, numImages, duration, size, top, left, zIndex} = details;
-    const [index, setIndex] = useState({value: startIndex}); //state to hold the current index of the image
+function Canvas({ details }) {
+    const { startIndex, numImages, duration, size, top, left, zIndex } = details;
+    const [index, setIndex] = useState({ value: startIndex }); //state to hold the current index of the image
     const canvasRef = useRef(null); //reference to the canvas element
 
     useGSAP(() => {
@@ -15,8 +15,8 @@ function Canvas({details}) {
             repeat: -1, //repeat the animation indefinitely
             ease: "linear", //smooth transition between values
             onUpdate: () => {
-                setIndex({value: Math.round(index.value)}); //update the index state with the current value of the animation and round it to the nearest integer such that it doesn't take decimal values
-            }, 
+                setIndex({ value: Math.round(index.value) }); //update the index state with the current value of the animation and round it to the nearest integer such that it doesn't take decimal values
+            },
         });
     });
 
@@ -24,13 +24,13 @@ function Canvas({details}) {
         const scale = window.devicePixelRatio; //get the device pixel ratio
         const canvas = canvasRef.current; //reference to the canvas element
         const ctx = canvas.getContext('2d'); //2d context for drawing on the canvas
-            
+
         const image = new Image(); //create a new image element
         image.src = canvasImages[index.value]; //set the source of the image to the current index of the image
-        
+
         image.onload = () => {
             canvas.width = canvas.offsetWidth * scale; //set the width of the canvas to the width of the image multiplied by the device pixel ratio
-            canvas.height = canvas.offsetHeight * scale ; //set the height of the canvas to the height of the image multiplied by the device pixel ratio
+            canvas.height = canvas.offsetHeight * scale; //set the height of the canvas to the height of the image multiplied by the device pixel ratio
             canvas.style.width = canvas.offsetWidth + "px"; //set the width of the canvas to the width of the image multiplied by the device pixel ratio
             canvas.style.height = canvas.offsetHeight + "px"; //set the height of the canvas to the height of the image multiplied by the device pixel ratio
 
@@ -39,18 +39,20 @@ function Canvas({details}) {
         };
     }, [index]);
     return (
-    <canvas 
-    ref ={canvasRef} 
-    className="absolute"
-    style={{
-    width: `${size * 1.3}px`, 
-    height: `${size * 1.3}px`,
-    top: `${top}%`,
-    left: `${left}%`,
-    zIndex: `${zIndex}`,
-}}
-    id="canvas" 
-    ></canvas>
+        <canvas
+            data-scroll
+            data-scroll-speed={Math.random().toFixed(1)} //random scroll speed for each canvas
+            ref={canvasRef}
+            className="absolute"
+            style={{
+                width: `${size * 1.3}px`,
+                height: `${size * 1.3}px`,
+                top: `${top}%`,
+                left: `${left}%`,
+                zIndex: `${zIndex}`,
+            }}
+            id="canvas"
+        ></canvas>
     );
 }
 
